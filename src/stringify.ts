0641571f290b786680b62261cad40ae61c38076f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-04-05 04:05:31 +0000

    fix: stringify should clone nodes

--- src/stringify.ts
+++ src/stringify.ts
@@ -1,5 +1,5 @@
 export function stringify(nodes: Node[]): string {
   const div = document.createElement('div')
-  nodes.forEach(x => div.append(x))
+  nodes.forEach(x => div.append(x.cloneNode(true)))
   return div.innerHTML
 }
