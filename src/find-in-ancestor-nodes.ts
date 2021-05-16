77d4cc37fd9d8ec133bb5a34ccaf6b11e79e9277
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-05-16 16:41:08 +0000

    chore: upgrade dependencies

--- src/find-in-ancestor-nodes.ts
+++ src/find-in-ancestor-nodes.ts
@@ -1,14 +1,9 @@
 import { traverseAncestorNodes } from './traverse-ancestor-nodes'
-import * as IO from 'iterable-operator'
+import { find } from 'iterable-operator'
 
 export function findInAncestorNodes(
   node: Node
 , predicate: (node: Node) => unknown
 ): (Node & ParentNode) | null {
-  try {
-    return IO.find(traverseAncestorNodes(node), node => predicate(node))
-  } catch (e) {
-    if (e instanceof IO.RuntimeError) return null
-    throw e
-  }
+  return find(traverseAncestorNodes(node), node => predicate(node)) ?? null
 }
--- src/find-in-descendant-nodes.ts
+++ src/find-in-descendant-nodes.ts
@@ -1,14 +1,9 @@
 import { traverseDescendantNodes } from './traverse-descendant-nodes'
-import * as IO from 'iterable-operator'
+import { find } from 'iterable-operator'
 
 export function findInDescendantNodes(
   node: Node
 , predicate: (node: ChildNode) => unknown
 ): ChildNode | null {
-  try {
-    return IO.find(traverseDescendantNodes(node), node => predicate(node))
-  } catch (e) {
-    if (e instanceof IO.RuntimeError) return null
-    throw e
-  }
+  return find(traverseDescendantNodes(node), node => predicate(node)) ?? null
 }
