53bbc53f868d8736f7ed3c73c4568164941401b7
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-12-12 11:21:46 +0000

    refactor: move functions

--- src/previous-element-sibling.ts
+++ src/previous-element-sibling.ts
@@ -8,20 +8,20 @@ export function previousElementSibling(
   assert(Number.isInteger(distance), 'parameter distance must be an integer')
   assert(distance >= 1, 'parameter distance must be greater than or equal to 1')
 
-  return _previousElementSibling(node, distance)
-}
+  return previousElementSibling(node, distance)
 
-function _previousElementSibling(node: Node, distance: number = 1): Element | undefined {
-  const currentNode = node.previousSibling
-  if (!currentNode) return undefined
+  function previousElementSibling(node: Node, distance: number = 1): Element | undefined {
+    const currentNode = node.previousSibling
+    if (!currentNode) return undefined
 
-  if (isElement(currentNode)) {
-    if (distance === 1) {
-      return currentNode as Element
+    if (isElement(currentNode)) {
+      if (distance === 1) {
+        return currentNode as Element
+      } else {
+        return previousElementSibling(currentNode, distance - 1)
+      }
     } else {
-      return _previousElementSibling(currentNode, distance - 1)
+      return previousElementSibling(currentNode, distance)
     }
-  } else {
-    return _previousElementSibling(currentNode, distance)
   }
 }
--- src/previous-sibling.ts
+++ src/previous-sibling.ts
@@ -4,16 +4,16 @@ export function previousSibling(node: Node, distance: number = 1): ChildNode | u
   assert(Number.isInteger(distance), 'parameter distance must be an integer')
   assert(distance >= 1, 'parameter distance must be greater than or equal to 1')
 
-  return _previousSibling(node, distance)
-}
+  return previousSibling(node, distance)
 
-function _previousSibling(node: Node, distance: number = 1): ChildNode | undefined {
-  const currentNode = node.previousSibling
-  if (!currentNode) return undefined
+  function previousSibling(node: Node, distance: number = 1): ChildNode | undefined {
+    const currentNode = node.previousSibling
+    if (!currentNode) return undefined
 
-  if (distance === 1) {
-    return currentNode
-  } else {
-    return _previousSibling(currentNode, distance - 1)
+    if (distance === 1) {
+      return currentNode
+    } else {
+      return previousSibling(currentNode, distance - 1)
+    }
   }
 }
