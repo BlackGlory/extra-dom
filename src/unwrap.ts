b94ce27d84c3fa56cfdbb998aa4af57030529c21
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-02-23 05:56:00 +0000

    docs: improve readability

--- src/unwrap.ts
+++ src/unwrap.ts
@@ -4,7 +4,9 @@ import { map, flatten, toArray } from 'iterable-operator'
 export function unwrap(node: Node, predicate: (node: Node) => unknown): Node[] {
   return flatMap(node, node => {
     if (predicate(node)) {
-      // It is `return Array.from(node.childNodes).flatMap(node => unwrap(node, predicate)`
+      // Array
+      //   .from(node.childNodes)
+      //   .flatMap(node => unwrap(node, predicate)
       return toArray(flatten(map(node.childNodes, node => unwrap(node, predicate))))
     } else {
       return [node]
